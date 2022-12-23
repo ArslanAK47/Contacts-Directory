1c5a8b602bfff549cb01891bbbf8b690b7b855d6
arslan khalid <arslan.khalid@purelogics.net>
2022-12-23 20:19:32 +0500

    added getcontacts api

--- book-backend/controllers/contact-controller.js
+++ book-backend/controllers/contact-controller.js
@@ -8,7 +8,15 @@ const saveContact = async (req, res ) => {
         Email,
         Password
     })
-
+    let existinguser;
+    try{
+        existinguser = await Contact.findOne({Email});
+    }catch(e){
+        console.error(e);
+    }
+    if (existinguser){
+        return res.status(400).json({message: "Contact already exists"})
+    }
     try{
         contact.save()
         return res.status(201).json(contact)
@@ -17,4 +25,18 @@ const saveContact = async (req, res ) => {
     }
 }
 
-module.exports = {saveContact}
+
+const getContacts = async (req, res) => {
+    let users;
+    try{
+        users = await Contact.find()
+    } catch(err) {
+        console.error(err)
+    }
+    if(!users){
+        return res.status(404).json({message: "Users not found"})
+    }
+    return res.status(200).json({users})
+}
+
+module.exports = {saveContact, getContacts}
--- book-backend/routes/contact-route.js
+++ book-backend/routes/contact-route.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const { saveContact } = require("../controllers/contact-controller");
+const { saveContact, getContacts } = require("../controllers/contact-controller");
 const contactRouter = express.Router();
 
+
 contactRouter.post("/add", saveContact);
+contactRouter.get("/users", getContacts);
 
 
 
